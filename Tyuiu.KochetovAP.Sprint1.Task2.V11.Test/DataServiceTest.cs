0d0eacbae777996a565737322d86a489051033a1
andreykochetov <oliverr223344@gmail.com>
2025-10-01 02:50:33 +0600

    Task3

---
+++ Tyuiu.KochetovAP.Sprint1.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KochetovAP.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint1.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double area = TriangleArea(-2, 5, 1, 7, 5, -3);
+            Assert.Equals(19, Math.Round(area, 0));
+        }
+    }
+}
---
+++ Tyuiu.KochetovAP.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.KochetovAP.Sprint1.Task3.V11.Lib;
+
+namespace Tyuiu.KilikaevRV.Sprint1.Task3.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                DataService ds = new DataService();
+                Console.Title = "Спринт #1| Выполнил: Кочетов А.П | ИБКСБ-25-1";
+
+                //Длинна строки 75 символов
+                Console.WriteLine("********************************************************************************************************");
+                Console.WriteLine("Спринт #1");
+                Console.WriteLine("Тема: Базовые навыки работы в C#");
+                Console.WriteLine("Задание #3");
+                Console.WriteLine("Вариант #12");
+                Console.WriteLine("Выполнил: Киликаев Роман Валерьевич | ИБКСБ-25-1");
+                Console.WriteLine("********************************************************************************************************");
+                Console.WriteLine("УСЛОВИЕ:");
+                Console.WriteLine("Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.");
+                Console.WriteLine("и печатает результат на экране.");
+                Console.WriteLine("");
+                Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ:");
+
+                double x;
+                double y;
+
+                Console.WriteLine("Введите значение x:");
+                x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите значение y:");
+                y = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("********************************************************************************************************");
+
+
+                Console.WriteLine(" ответ = " + ds.TriangleArea(x, y));
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
