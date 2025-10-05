f4f22b59a6ec6ef39fb7a825564000835c9673c5
andreykochetov <oliverr223344@gmail.com>
2025-10-05 14:58:11 +0600

    Add project files.

---
+++ Tyuiu.KochetovAP.Sprint1.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.KochetovAP.Sprint1.Task3.V11.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint1.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x1 = -2;
+            double y1 = -5;
+            double x2 = 1;
+            double y2 = 7;
+            double x3 = 5;
+            double y3 = -3;
+            var res = ds.TriangleArea(x1, y1, x2, y2, x3, y3);
+            Assert.AreEqual(19, res);
+
+
+        }
+    }
+}
---
+++ Tyuiu.KochetovAP.Sprint1.Task3.V11/Program.cs
@@ -0,0 +1,58 @@
+﻿using Tyuiu.KochetovAP.Sprint1.Task3.V11.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint1.Task3.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                DataService ds = new DataService();
+                Console.Title = "Спринт #1| Выполнил: Кочетов А.П | ИБКСБ-25-1";
+
+                //Длинна строки 75 символов
+                Console.WriteLine("********************************************************************************************************");
+                Console.WriteLine("Спринт #1");
+                Console.WriteLine("Тема: Базовые навыки работы в C#");
+                Console.WriteLine("Задание #3");
+                Console.WriteLine("Вариант #11");
+                Console.WriteLine("Выполнил: Кочетов Андрей Павлович | ИБКСБ-25-1");
+                Console.WriteLine("********************************************************************************************************");
+                Console.WriteLine("УСЛОВИЕ:");
+                Console.WriteLine("Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.");
+                Console.WriteLine("и печатает результат на экране.");
+                Console.WriteLine("");
+                Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ:");
+
+                double x1;
+                double y1;
+                double x2;
+                double y2;
+                double x3;
+                double y3;
+
+                Console.WriteLine("Введите значение x1:");
+                x1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите значение y1:");
+                y1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите значение x2:");
+                x2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите значение y2:");
+                y2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите значение x3:");
+                x3 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите значение y3:");
+                y3 = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("********************************************************************************************************");
+
+
+                Console.WriteLine(" ответ = " + ds.TriangleArea(x1, y1, x2, y3, x3, y3));
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
