dac72f8000ad464951c1ea618709a3162f1fe726
andreykochetov <oliverr223344@gmail.com>
2025-11-26 19:48:50 +0600

    Task1

--- Tyuiu.KochetovAP.Sprint1.Task1.V11.Test/DataServiceTest.cs
+++ Tyuiu.KochetovAP.Sprint1.Task1.V11.Test/DataServiceTest.cs
@@ -7,12 +7,36 @@ namespace Tyuiu.KochetovAP.Sprint1.Task1.V11.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void ValidCalculate()
+        public void TestMethod1()
         {
             DataService ds = new DataService();
+
+ 
             double result = ds.Calculate(6, 2);
             double wait = 2.5;
             Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+
+
+            double result = ds.Calculate(12, 3);
+            double wait = 3.333;
+            Assert.AreEqual(wait, result, 0.001); 
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+
+
+            double result = ds.Calculate(1, 1);
+            double wait = 0.833;
+            Assert.AreEqual(wait, result, 0.001);
         }
     }
 }
