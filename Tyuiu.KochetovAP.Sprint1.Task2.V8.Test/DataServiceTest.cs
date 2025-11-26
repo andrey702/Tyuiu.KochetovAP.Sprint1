007701e493f54dc14156e3d4cbb80845266e65fb
andreykochetov <oliverr223344@gmail.com>
2025-11-26 19:55:08 +0600

    Task2

---
+++ Tyuiu.KochetovAP.Sprint1.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KochetovAP.Sprint1.Task2.V8.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint1.Task2.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculatePerimetr()
+        {
+            DataService ds = new DataService();
+            int result1 = ds.CalculatePerimetr(5, 3);
+            int wait1 = 16;
+            Assert.AreEqual(wait1, result1);
+
+            int result2 = ds.CalculatePerimetr(10, 7);
+            int wait2 = 34;
+            Assert.AreEqual(wait2, result2);
+
+            int result3 = ds.CalculatePerimetr(4, 4);
+            int wait3 = 16;
+            Assert.AreEqual(wait3, result3);
+        }
+    }
+}
---
+++ Tyuiu.KochetovAP.Sprint1.Task2.V8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Tyuiu.KochetovAP.Sprint1.Task2.V8.Lib;
+
+namespace Tyuiu.KochetovAP.Sprint1.Task2.V8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Кочетов А. П. | ИБКСб-25-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Кочетов Андрей Павлович | ИБКСб-25-1                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("* Формулировка задания: Известны длины стороны прямоугольника.            *");
+            Console.WriteLine("* Вычислить периметр прямоугольника.                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите длину первой стороны прямоугольника (целое число): ");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите длину второй стороны прямоугольника (целое число): ");
+            int side2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int result = ds.CalculatePerimetr(side1, side2);
+            Console.WriteLine($"Периметр прямоугольника со сторонами {side1} и {side2} = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
